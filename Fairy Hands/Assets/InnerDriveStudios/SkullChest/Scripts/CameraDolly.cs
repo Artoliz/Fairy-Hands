f9220b61aa2ef725fbfdf85e8313f34118d32aa0
Charles Descoust <charles.descoust@epitech.eu>
2020-04-07 17:18:45 +0900

    CartoonFxPack

---
+++ Fairy Hands/Assets/InnerDriveStudios/SkullChest/Scripts/CameraDolly.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraDolly : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, rotationSpeed, 0);
+	}
+}
