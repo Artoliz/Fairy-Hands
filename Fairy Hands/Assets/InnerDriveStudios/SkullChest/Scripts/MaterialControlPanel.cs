1e82f0290c184898312c84c58b43e626db630050
Charles Descoust <charles.descoust@epitech.eu>
2020-04-07 17:18:59 +0900

    CartoonFxPack

---
+++ Fairy Hands/Assets/InnerDriveStudios/SkullChest/Scripts/MaterialControlPanel.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Simple material swapper.
+ * 
+ * @author J.C. Wichman - Inner Drive Studios
+ */
+public class MaterialControlPanel : MonoBehaviour {
+
+	[SerializeField] private GameObject target;
+	[SerializeField] private Text materialText;
+	[SerializeField] private Material[] materials;
+	private MeshRenderer[] renderers;
+	private int _currentMaterialIndex = 0;
+	
+
+	private void Awake()
+	{
+		renderers = target.GetComponentsInChildren<MeshRenderer>();
+	}
+
+	private void Update()
+	{
+		//update the material info if required
+		if (materialText != null)
+		{
+			if (materials.Length == 0)
+			{
+				materialText.text = "-";
+			}
+			else
+			{
+				if (_currentMaterialIndex >= 0)
+				{
+					materialText.text = "" + (_currentMaterialIndex + 1) + "/" + materials.Length;
+				}
+				else
+				{
+					materialText.text = "R";
+				}
+			}
+		}
+	}
+
+	public void NextMaterial()
+	{
+		if (materials.Length == 0) return;
+		_currentMaterialIndex = (_currentMaterialIndex + 1) % materials.Length;
+		updateMaterial();
+	}
+
+	public void PreviousMaterial()
+	{
+		if (materials.Length == 0) return;
+		_currentMaterialIndex = (_currentMaterialIndex - 1 + materials.Length) % materials.Length;
+		updateMaterial();
+	}
+
+	private void updateMaterial()
+	{
+		for (int i = 0; i < renderers.Length; i++)
+		{
+			renderers[i].sharedMaterial = 
+				materials[_currentMaterialIndex>-1?_currentMaterialIndex:Random.Range(0, materials.Length)];
+		}
+	}
+
+}
---
+++ Fairy Hands/Assets/InnerDriveStudios/SkullChest/Scripts/SkullChest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class ChestState {
+	public Vector3 angle;
+	public AudioClip sound;
+	public float speed;
+}
+
+public class SkullChest : MonoBehaviour
+{
+	public List<ChestState> targetAngles;
+	public Transform top;
+	private AudioSource audioSource;
+	[Tooltip("Allows you to limit the angle to the camera under which a mousedown is accepted. -1 is from everywhere, 0 is from 90 degrees to the side and upward, 0-1 is limited to a cone to the front.")]
+	public float acceptLeeway = 0;
+
+	private void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	private void OnMouseDown()
+	{
+		float dot = Vector3.Dot(transform.right.normalized, (Camera.main.transform.position - transform.position).normalized);
+		if (dot < acceptLeeway) return;
+
+		if (targetAngles != null && targetAngles.Count > 0)
+		{
+			targetAngles.Add(targetAngles[0]);
+			targetAngles.RemoveAt(0);
+
+			AudioClip clip = targetAngles[0].sound;
+			if (clip != null)
+			{
+				audioSource.pitch = Random.Range(0.8f, 1.1f);
+				audioSource.volume = Random.Range(0.8f, 1.0f);
+				audioSource.PlayOneShot(clip);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		ChestState state = targetAngles[0];
+		top.transform.localRotation = Quaternion.Slerp(top.transform.localRotation, Quaternion.Euler(state.angle), state.speed * 0.5f * Time.deltaTime);
+    }
+}
