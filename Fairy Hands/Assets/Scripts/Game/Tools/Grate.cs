a03d1f62f33a962dbd9a9c26cdc3a6d92be7aa41
Charles Descoust <charles.descoust@epitech.eu>
2020-04-28 11:52:15 +0900

    Game

--- Fairy Hands/Assets/Scripts/Game/Tools/Grate.cs
+++ Fairy Hands/Assets/Scripts/Game/Tools/Grate.cs
@@ -5,14 +5,14 @@ using Valve.VR.InteractionSystem;
 
 public class Grate : MonoBehaviour
 {
-    public float Magnitude = 3.0f;
+    public float Magnitude = 2.0f;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Ingredient"))
         {
             var velocityEstimator = GetComponentInParent<VelocityEstimator>();
-            if (velocityEstimator && velocityEstimator.GetVelocityEstimate().magnitude >= Magnitude)
+            if (velocityEstimator)
             {
                 var modifier = other.gameObject.GetComponent<Modifier>();
                 if (modifier)
